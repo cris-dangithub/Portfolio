8796a2cf8ecf371302d80e055f0bdbba3c8c5982
cris-dangithub <cristiandaniel8080@gmail.com>
2022-10-31 17:40:54 -0500

    Experiencia terminada, falta solo hacer que los numeros incrementen cuando se llegue el scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,21 @@
 const iconMenu = document.querySelector('.main__navbar .navbar__icon')
 const contentMenu = document.querySelector(".main__navbar ul")
+const contentAnchorIconMenu = document.querySelector(".main__navbar ul a svg")
+const anchorMenu = document.querySelectorAll(".navbar__links li a") 
 const mainContainer = document.querySelector(".main .main__container")
 
+
+console.log(anchorMenu)
 /* Navbar */
+function quiteMenu(active) {
+    if (active) {
+        contentMenu.classList.toggle("navbar__show")
+        mainContainer.classList.toggle("main__containerShowMenu")
+        active = false
+    }
+    return active
+}
+
 let activeMenu = false;
 iconMenu.addEventListener('click', function () {
         contentMenu.classList.toggle("navbar__show")
@@ -11,12 +24,39 @@ iconMenu.addEventListener('click', function () {
     }
 )
 
-mainContainer.addEventListener('click', function() {
-        if (activeMenu) {
-            contentMenu.classList.toggle("navbar__show")
-            mainContainer.classList.toggle("main__containerShowMenu")
-            activeMenu = false
-        }
+mainContainer.addEventListener('click', function () {
+        activeMenu = quiteMenu(activeMenu)
     }
 )
 
+contentAnchorIconMenu.addEventListener('click' , function () {
+        activeMenu = quiteMenu(activeMenu)
+    }
+)
+
+for (let i = 0; i < anchorMenu.length; i++) {
+        anchorMenu[i].addEventListener('click', function () {
+            activeMenu = quiteMenu(activeMenu)
+        }
+    )
+}
+
+
+
+/* Habilidades */
+window.addEventListener('scroll', function()  {
+        const containerHabilities = document.querySelector('.container__skills .skills__values');
+        let elements = document.querySelectorAll('.bar__progress')
+        let screenSize = window.innerHeight;
+        
+        for (element of elements) {
+            condition1 = containerHabilities.getBoundingClientRect().top < screenSize
+            condition2 = containerHabilities.getBoundingClientRect().bottom > 0
+            if(condition1 && condition2) {
+                element.classList.add('visible');
+            } else {
+                element.classList.remove('visible');
+            }
+        }
+    }
+);
