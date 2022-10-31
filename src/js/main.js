0778b6944fe47ece7722c867a6220877e494d544
cris-dangithub <cristiandaniel8080@gmail.com>
2022-10-31 11:43:00 -0500

    body tambien ayuda a cerrar menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,21 @@ const iconMenu = document.querySelector('.main__navbar .navbar__icon')
 const contentMenu = document.querySelector(".main__navbar ul")
 const mainContainer = document.querySelector(".main .main__container")
 
-console.log(console.log(contentMenu.classList))
+/* Navbar */
+let activeMenu = false;
 iconMenu.addEventListener('click', function () {
-        console.log('holamundo')
-        console.log(contentMenu.classList.toggle("navbar__show"))
-        console.log(mainContainer.classList.toggle("main__containerShowMenu"))
+        contentMenu.classList.toggle("navbar__show")
+        mainContainer.classList.toggle("main__containerShowMenu")
+        activeMenu === false ? activeMenu = true : activeMenu = false
     }
 )
+
+mainContainer.addEventListener('click', function() {
+        if (activeMenu) {
+            contentMenu.classList.toggle("navbar__show")
+            mainContainer.classList.toggle("main__containerShowMenu")
+            activeMenu = false
+        }
+    }
+)
+
